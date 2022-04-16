92430ec9bc5c68ba444cda21f1b925e171d4f22c
AlekseiZhukov <alex__zhukov@mail.ru>
2022-04-16 16:37:57 +0300

    finished homework#03

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,30 +1,41 @@
 import React from "react";
-import s from './Heading.module.css';
+import s from './Heading.module.scss';
 import cn from 'classnames';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const Heading = ({
                      children,
                      level,
                      className,
+                     backLine
 
                  }) => {
 
     const el = `h${level}`
 
     return React.createElement(el, {
-        className: cn(s.root, className, s[`level${level}`])
+        className: cn(
+            s.root,
+            className,
+            s[`level${level}`],
+            {
+                [s.backline]: backLine
+            }
+
+        )
     }, children);
 };
 
 Heading.defaultProps = {
     level: 1,
+    backLine: false
 };
 
 Heading.propTypes = {
     className: PropTypes.string,
     level: PropTypes.oneOf([1, 2, 3, 4, 5]),
-    children: PropTypes.node
+    children: PropTypes.node,
+    backLine: PropTypes.bool,
 };
 
 export default Heading;
